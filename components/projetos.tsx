812d8ef8122fc111f859462a57d7d5a8d3df1783
Gabriel Alves dos Santos <gabrielalvesdossantos16@gmail.com>
2023-08-02 15:23:06 -0300

    cards

--- components/projetos.tsx
+++ components/projetos.tsx
@@ -2,7 +2,7 @@ import './cards.css'
 
 export default function Cards(){
     return(
-           <div>
+           <div className='cards'>
              <div className="card">
         <div className='content'>
         <div className="cardname">Site de Produtos</div>
@@ -12,7 +12,17 @@ export default function Cards(){
           <a href='https://github.com/GabrielAlSant/BD-Produtos'><div className="cardlink">Ver Código do Back-end</div></a>
         </div>
         </div>
-           </div>
+
+        <div className='card'>
+        <div className='content'>
+        <div className="cardname">Site de Produtos, com clientes, vendas e funcionários</div>
+         <div className="cardresume">Site feito com Front-End feito em Nextjs, que conta com a manipulação de imagens e banco de dados relacional, Back-End desenvolvido em Node.js/Postgres</div>
+          <a href='https://mysistemstore.vercel.app'><div className="cardlink">Ver Aplicação</div></a>
+          <a href='https://github.com/GabrielAlSant/FrontEnd-ProjetoNextJSSequelize-PDF'><div className="cardlink">Ver Código do Front-end</div></a>
+          <a href='https://github.com/GabrielAlSant/BackEnd-WebSite-PDFandIMG--NEXTjs-Sequelize-express-'><div className="cardlink">Ver Código do Back-end</div></a>
+        </div>
+        </div>
+        </div>
 
     )
 }
